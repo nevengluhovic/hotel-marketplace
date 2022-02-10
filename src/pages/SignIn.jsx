a2ae531040df6cd15c9554cfd4039718589c34f1
Neven Gluhovic <nevengluhovic@gmail.com>
2022-02-10 20:17:44 +0100

    sign-up and auth

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+
 import { ReactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRightIcon.svg";
 import visibilityIcon from "../assets/svg/visibilityIcon.svg";
 
