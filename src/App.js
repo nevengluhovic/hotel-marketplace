1009502b97694d949738212bb6b8fe91584ac783
Neven Gluhovic <nevengluhovic@gmail.com>
2022-02-10 13:28:16 +0100

    React router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Profile from "./pages/Profile";
+import Offers from "./pages/Offers";
+import Explore from "./pages/Explore";
+import ForgotPassword from "./pages/ForgotPassword";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 
 function App() {
   return (
-    <div className="App">
-      <h1>House Marketplace</h1>
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Explore />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/profile" element={<SignUp />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const SignUp = () => {
-  return <div>SignUp</div>;
+  return <div>Sign Up</div>;
 };
 
 export default SignUp;
